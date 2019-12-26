f3b074eb4523bbda3de367c84a4c2b2d778a13fd
= <isaabqari@gmail.com>
2019-12-26 19:42:00 +0700

    multiple controller

--- src/main/java/com/fluex404/controller/HelloController.java
+++ src/main/java/com/fluex404/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.fluex404.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +17,13 @@ public class HelloController {
     @RequestMapping("/helloagain")
     public String display(){
         return "final";
+    }
+    @GetMapping("/hello1")
+    public String getHello1(){
+        return "hello1";
+    }
+    @GetMapping("/hello2")
+    public String getHello2(){
+        return "hello2";
     }
 }
--- src/main/webapp/WEB-INF/jsp/hello1.jsp
+++ src/main/webapp/WEB-INF/jsp/hello1.jsp
@@ -2,18 +2,12 @@
   Created by IntelliJ IDEA.
   User: fluex404
   Date: 12/26/19
-  Time: 7:21 PM
+  Time: 7:39 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-<head>
-    <title>Spring MVC</title>
-</head>
 <body>
-    <h3>Welcome to Spring MVC</h3><br/>
-    <a href="hello">Click here...</a><br/>
-    <a href="hello1">Hello 1</a><br/>
-    <a href="hello2">Hello 2</a><br/>
+    hello1
 </body>
 </html>
