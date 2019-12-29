cf10e20f7586dab35c5cd596c84a243cf261bc06
= <isaabqari@gmail.com>
2019-12-29 09:06:39 +0700

    handle exception

--- src/main/java/com/fluex404/restcontroller/EmployeeRestController.java
+++ src/main/java/com/fluex404/restcontroller/EmployeeRestController.java
@@ -2,10 +2,13 @@ package com.fluex404.restcontroller;
 
 import com.fluex404.model.Employee;
 import com.fluex404.utils.EmpRestURIContats;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.*;
-import java.util.logging.Logger;
 
 @RestController
 public class EmployeeRestController {
@@ -22,8 +25,27 @@ public class EmployeeRestController {
         return emp;
     }
     @GetMapping(EmpRestURIContats.GET_EMP)
-    public Employee getEmployee(@PathVariable("id") int empId){
-        return empData.get(empId);
+    public ModelMap getEmployee(@PathVariable("id") int empId) throws Exception {
+//        return empData.get(empId);
+        ModelMap m = new ModelMap();
+        try{
+            if(empId == 1) {
+                throw new Exception("Error tidak ditemukan");
+            } else if(empId == 2) {
+                throw new SQLException("SQLException, id="+empId);
+            } else if(empId == 3) {
+                throw new IOException("IOException, id=" + empId);
+            } else {
+                throw new Exception("Generic Exception, id="+empId);
+            }
+        }catch(Exception e) {
+
+            m.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+            m.put("message", e.getMessage());
+            m.put("data", null);
+            return m;
+
+        }
     }
     @GetMapping(EmpRestURIContats.GET_ALL_EMP)
     public List<Employee> getAllEmployees(){
---
+++ src/main/webapp/resources/404.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: fluex404
+  Date: 12/29/19
+  Time: 7:40 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>ERROR</title>
+</head>
+<body>
+
+</body>
+</html>
