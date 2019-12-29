6f3a29f9a6397a6a66f4ec1977b00dc8d8eed461
= <isaabqari@gmail.com>
2019-12-29 09:38:29 +0700

    responsemapping

---
+++ src/main/java/com/fluex404/controller/RequestMappingController.java
@@ -0,0 +1,22 @@
+package com.fluex404.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RequestMappingController {
+    @GetMapping(value = {"/request1","/request2"})
+            //produces = {"application/json","application/xml"},
+            //consumes="text/html") //headers = "name=isa"
+    @ResponseBody
+    public ModelMap getRequest(){
+        ModelMap m = new ModelMap();
+
+        m.put("data", "ini datanya");
+
+        return m;
+    }
+}
